7e7dd05ebfc1266702d339638e5bf2d46a0d3b56
alan2325 <alalkumar06@gmail.com>
2024-06-26 10:50:39 +0530

    done

---
+++ reverse.py
@@ -0,0 +1,17 @@
+l=['abc','efg','abc']
+find=str(input("Enter the no : "))
+c=l.count(find)
+pos=0
+while c>0:
+    p=l.index(find,pos)
+    l.reverse
+    print()
+    
+    pos=p+1
+    c-=1
+    
+if find in l:
+      print()  
+else:
+        print("INVALID INPUT !")
+    

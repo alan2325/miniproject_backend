7568e0630a378e20a347eda3ffd93c08e579fa44
alan2325 <alalkumar06@gmail.com>
2024-06-27 11:39:14 +0530

    done

--- homework.py
+++ homework.py
@@ -1,7 +1,6 @@
-mark=['reg','name','eng','mal','maths','bio','phy','che'
-    [12345,'alal',50,40,55,34,73,47]
-    [67890,'aro',43,65,45,51,33,63]
-    ]
+L=[['reg','name','eng','mal','maths','bio','phy','che'],
+    [12345,'alal',50,40,55,34,73,47],
+    [67890,'aro',43,65,45,51,33,63]]
 while True:
     print("\n1.Login \n2.cancle")
     a=int(input("Enter your choice : "))
@@ -12,9 +11,18 @@ while True:
         reg=int(input("Enter regester number : "))
         name=str(input("Enter name : ")) 
         f=0
-        for i in mark:
+        for i in L:
             if reg==i[0] and name==i[1]: 
                 f=1
                 print("You have successfully logined")
-                for row in mark:
-                    print("\t".join(map(str,row)))
+                for row in L:
+                    # print("\t".join(map(str,row)))
+                    print("Marks are : ",i[2],i[3],i[4],i[5],i[6],i[7])
+
+                    break
+            if f==0:
+                print("ID not found")
+    else:
+                    
+                print("INVALID INPUT !")
+    

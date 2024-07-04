a1b7c6266c7bec60ffe4d6c77eb69293485e8304
alan2325 <alalkumar06@gmail.com>
2024-07-04 23:20:10 +0530

    done

--- task_2.py
+++ task_2.py
@@ -1,9 +1,9 @@
-data=[{'op':'yes','uname':'cab1','pswd':987,'email':'cab1@gmail.com','pno':'123456700','loc':'ktm','tno':'kl5b123'},
-      {'op':'yes','uname':'cab2','pswd':876,'email':'cab2@gmail.com','pno':'998766789','loc':'ktm','tno':'kl6b123'},
-      {'op':'yes','uname':'cab3','pswd':765,'email':'cab3@gmail.com','pno':'123467812','loc':'ktm','tno':'kl7b123'},
-      {'op':'no','uname':'alan','pswd':123,'email':'al@gmail.com','pno':'123456789','loc':'ktm'},
-      {'op':'no','uname':'bla','pswd':234,'email':'bla@gmail.com','pno':'987654321','loc':'ktm'},
-      {'op':'no','uname':'hlo','pswd':345,'email':'hlo@gmail.com','pno':'123454321','loc':'ktm'}]
+data=[{'op':'yes','uname':'cab1','pswd':987,'email':'cab1@gmail.com','pno':'123456700','livloc':'ekm','tno':'kl5b123'},
+      {'op':'yes','uname':'cab2','pswd':876,'email':'cab2@gmail.com','pno':'998766789','livloc':'ktm','tno':'kl6b123'},
+      {'op':'yes','uname':'cab3','pswd':765,'email':'cab3@gmail.com','pno':'123467812','livloc':'vkm','tno':'kl7b123'},
+      {'op':'no','uname':'user1','pswd':123,'email':'u1@gmail.com','pno':'123456789','livloc':'ktm','headloc':'vkm'},
+      {'op':'no','uname':'user2','pswd':234,'email':'u2@gmail.com','pno':'987654321','livloc':'vkm','headloc':'vkm'},
+      {'op':'no','uname':'user3','pswd':345,'email':'u3@gmail.com','pno':'123454321','livloc':'ekm','headloc':'vkm'},]
 
 while True:
     # Display the menu
@@ -18,16 +18,16 @@ while True:
             pswd=int(input("Enter password : "))
             email=str(input("Enter your place : "))
             pno=int(input("Enter your phone number : "))
-            loc=str(input("Share your location : "))
+            livloc=str(input("Share your location : "))
             tno=str(input("Enter your taxi regestration number : "))
-            data.append({'name':uname,'pswd':pswd,'email':email,'pno':pno,'loc':loc,'tno':tno})
+            data.append({'name':uname,'pswd':pswd,'email':email,'pno':pno,'livloc':livloc,'tno':tno})
         elif choice == 2:
             uname=str(input("Enter the name : "))
             pswd=int(input("Enter password : "))
             email=str(input("Enter your place : "))
             pno=int(input("Enter your phone number : "))
-            loc=str(input("Share your location : "))
-            data.append({'name':uname,'age':pswd,'place':email,'pno':pno,'loc':loc})
+            livloc=str(input("Share your current location : "))
+            data.append({'name':uname,'age':pswd,'place':email,'pno':pno,'livloc':livloc})
         else:
             print("Invalid input")
     if choice == 2:

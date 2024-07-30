f3f047ff7d5e670246b51037e07775c77e65f8d4
alan2325 <alalkumar06@gmail.com>
2024-07-30 09:41:09 +0530

    done

--- task_5/main.py
+++ task_5/main.py
@@ -8,7 +8,7 @@ def main():
     user_manager = UserManager()
     
     while True:
-        print("\n1. Register\n2. Login\n3. Add Accessory\n4. View Accessories\n5. Exit")
+        print("\n1. Register\n2. Login\n3. Exit")
         choice = input("Enter your choice: ")
 
         if choice == '1':
@@ -22,7 +22,7 @@ def main():
             user = user_manager.login_user(username, password)
             if user:
                 while True:
-                    print("\n1. Borrow Accessory\n2. Return Accessory\n3. Logout")
+                    print("\n1. Borrow Book\n2. Return Book\n3. Logout")
                     user_choice = input("Enter your choice: ")
                     if user_choice == '1':
                         accessory_id = input("Enter accessory ID: ")
@@ -35,16 +35,16 @@ def main():
                     else:
                         print("Invalid choice.")
         
+        # elif choice == '3':
+        #     accessory_id = input("Enter accessory ID: ")
+        #     name = input("Enter accessory name: ")
+        #     price = float(input("Enter accessory price: "))
+        #     accessory_manager.add_accessory(accessory_id, name, price)
+        
+        # elif choice == '4':
+        #     accessory_manager.view_accessories()
+        
         elif choice == '3':
-            accessory_id = input("Enter accessory ID: ")
-            name = input("Enter accessory name: ")
-            price = float(input("Enter accessory price: "))
-            accessory_manager.add_accessory(accessory_id, name, price)
-        
-        elif choice == '4':
-            accessory_manager.view_accessories()
-        
-        elif choice == '5':
             break
         
         else:

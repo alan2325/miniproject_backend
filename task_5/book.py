c05a5bf8fb37f8348018f3950383ae4981de2aa1
alan2325 <alalkumar06@gmail.com>
2024-07-31 10:50:22 +0530

    done

--- task_5/book.py
+++
@@ -1,8 +0,0 @@
-class Book:
-    def _init_(self, title, author, copies):
-        self.title = title
-        self.author = author
-        self.copies = copies
-
-    def _str_(self):
-        return f"{self.title} by {self.author} - {self.copies} copies available"

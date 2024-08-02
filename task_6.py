9587d1399de1ec821352273a624c110697730d1d
alan2325 <alalkumar06@gmail.com>
2024-08-02 11:23:53 +0530

    add database

--- task_6.py
+++ task_6.py
@@ -1,8 +1,10 @@
 while True:
-    print('\n1. Register\n2. Login\n3. Exit')
+    print('\n1. Add\n2. Update\n3. Display\n4. Delete\n5. Exit')
     ch = int(input('Enter your choice: '))
 
     if ch == 1:
-        name=str(input())
+        name=str(input("Enter name of the product : "))
+        stock=int(input("Enter stock : "))
 
     elif ch == 2:
+        print()
